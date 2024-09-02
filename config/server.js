ba5df77648be6353828f5368516986cb5b4e658d
OlshePete <olshevskii.pv@gmail.com>
2024-09-02 20:44:49 +0300

    update url path

--- config/server.js
+++ config/server.js
@@ -1,10 +1,16 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+export default ({ env }) => ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: 'https://calca-toys.ru/cms',
+    app: {
+        keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+    locales: [{
+        code: 'ru',
+        iso: 'ru-RU',
+        file: 'ru.json',
+    }, ],
 });
