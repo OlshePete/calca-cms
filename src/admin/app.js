4b6e9f949441e38dc044fff61284dcaa69772bbc
OlshePete <olshevskii.pv@gmail.com>
2024-09-03 21:17:03 +0300

    update favicon

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,36 +1,44 @@
+import Favicon from "../extensions/favicon.png";
+import AuthLogo from "../extensions/logo.svg";
 export default {
-  config: {
-    locales: [
-      'ru',
-      // 'ar',
-      // 'fr',
-      // 'cs',
-      // 'de',
-      // 'dk',
-      // 'es',
-      // 'he',
-      // 'id',
-      // 'it',
-      // 'ja',
-      // 'ko',
-      // 'ms',
-      // 'nl',
-      // 'no',
-      // 'pl',
-      // 'pt-BR',
-      // 'pt',
-      // 'ru',
-      // 'sk',
-      // 'sv',
-      // 'th',
-      // 'tr',
-      // 'uk',
-      // 'vi',
-      // 'zh-Hans',
-      // 'zh',
-    ],
-  },
-  bootstrap(app) {
-    console.log(app);
-  },
+    config: {
+        auth: {
+            logo: AuthLogo,
+        },
+        head: {
+            favicon: Favicon,
+        },
+        locales: [
+            'ru',
+            // 'ar',
+            // 'fr',
+            // 'cs',
+            // 'de',
+            // 'dk',
+            // 'es',
+            // 'he',
+            // 'id',
+            // 'it',
+            // 'ja',
+            // 'ko',
+            // 'ms',
+            // 'nl',
+            // 'no',
+            // 'pl',
+            // 'pt-BR',
+            // 'pt',
+            // 'ru',
+            // 'sk',
+            // 'sv',
+            // 'th',
+            // 'tr',
+            // 'uk',
+            // 'vi',
+            // 'zh-Hans',
+            // 'zh',
+        ],
+    },
+    bootstrap(app) {
+        console.log(app);
+    },
 };
